badd60aaf43d4e902c4cd09168f7341546e8beb7
Олег Семенов <neroragno@mail.ru>
2018-06-25 12:34:00 +0700

    Исправлено на более корректное решение

--- lesson_002/09_shopping.py
+++ lesson_002/09_shopping.py
@@ -25,22 +25,60 @@ shops = {
         ],
 }
 
+shop_names = list(shops.keys())
+
+# sweets lists:
+
+cookie_list = [shops[shop_names[0]][0], shops[shop_names[1]][0], shops[shop_names[2]][0]]
+candy_list = [shops[shop_names[0]][1], shops[shop_names[1]][1], shops[shop_names[2]][1]]
+caramel_list = [shops[shop_names[0]][2], shops[shop_names[1]][2], shops[shop_names[2]][2]]
+cake_list = [shops[shop_names[0]][3], shops[shop_names[1]][3], shops[shop_names[2]][3]]
+
+# sweets prices per shop:
+
+cookie_prices = {cookie_list[0]['price']: shop_names[0],
+                 cookie_list[1]['price']: shop_names[1],
+                 cookie_list[2]['price']: shop_names[2]}
+candy_prices = {candy_list[0]['price']: shop_names[0],
+                candy_list[1]['price']: shop_names[1],
+                candy_list[2]['price']: shop_names[2]}
+caramel_prices = {caramel_list[0]['price']: shop_names[0],
+                  caramel_list[1]['price']: shop_names[1],
+                  caramel_list[2]['price']: shop_names[2]}
+cake_prices = {cake_list[0]['price']: shop_names[0],
+               cake_list[1]['price']: shop_names[1],
+               cake_list[2]['price']: shop_names[2]}
+
+# removing most expensive
+
+del cookie_prices[max(cookie_prices)]
+del candy_prices[max(candy_prices)]
+del caramel_prices[max(caramel_prices)]
+del cake_prices[max(cake_prices)]
+
+cookie_prices_list = list(cookie_prices)
+candy_prices_list = list(candy_prices)
+caramel_prices_list = list(caramel_prices)
+cake_prices_list = list(cake_prices)
+
+# result:
+
 sweets = {
     'печенье': [
-        {'shop': 'пятерочка', 'price': 9.99},
-        {'shop': 'ашан', 'price': 10.99},
+        {'shop': cookie_prices[cookie_prices_list[0]], 'price': cookie_prices_list[0]},
+        {'shop': cookie_prices[cookie_prices_list[1]], 'price': cookie_prices_list[1]},
     ],
     'конфеты': [
-        {'shop': 'магнит', 'price': 30.99},
-        {'shop': 'пятерочка', 'price': 32.99},
+        {'shop': candy_prices[candy_prices_list[0]], 'price': candy_prices_list[0]},
+        {'shop': candy_prices[candy_prices_list[1]], 'price': candy_prices_list[1]},
     ],
     'карамель': [
-        {'shop': 'магнит', 'price': 41.99},
-        {'shop': 'ашан', 'price': 45.99},
+        {'shop': caramel_prices[caramel_prices_list[0]], 'price': caramel_prices_list[0]},
+        {'shop': caramel_prices[caramel_prices_list[1]], 'price': caramel_prices_list[1]},
     ],
     'пирожное': [
-        {'shop': 'пятерочка', 'price': 59.99},
-        {'shop': 'магнит', 'price': 62.99},
+        {'shop': cake_prices[cake_prices_list[0]], 'price': cake_prices_list[0]},
+        {'shop': cake_prices[cake_prices_list[1]], 'price': cake_prices_list[1]},
     ],
 }
 
