f9466a1a6bd051b989e0c43ca4da7583c4885835
unknown <ybegator@yandex.ru>
2018-06-29 00:10:27 +0300

    Add lesson 3

---
+++ lesson_003/06_rainbow.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# (цикл for)
+
+import simple_draw as sd
+
+rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
+                  sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
+
+# TODO Нарисовать радугу: 7 линий толщиной 4 с шагом 5 из точки (50, 50) в точку (550, 550)
+
+
+# (усложненное задание, делать по желанию)
+# TODO Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
+# TODO поэкспериментировать с параметрами, что бы было красиво
+
+sd.pause()
---
+++ lesson_003/07_wall.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# (цикл for)
+import simple_draw
+
+# TODO Нарисовать стену из кирпичей. Размер кирпича - 100х50
+# TODO использовать вложенные циклы for
+
+
+simple_draw.pause()
