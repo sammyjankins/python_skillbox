a3351ed721328f5ff294baa45c35638ec8a46ce4
SammyJankins <neroragno@mail.ru>
2020-06-27 14:03:43 +0700

    Первая часть задания: Класс для упорядочивания из папки в папку Заготовка для упорядочивания из архива

--- lesson_009/03_files_arrange.py
+++ lesson_009/03_files_arrange.py
@@ -34,7 +34,57 @@ import os, time, shutil
 # Чтение документации/гугла по функциям - приветствуется. Как и поиск альтернативных вариантов :)
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
+import zipfile
+from pprint import pprint
+
+
+class FileArrange:
+
+    def __init__(self, source_name, destination_name):
+        self.source_name = source_name
+        self.destination_name = destination_name
+        self.full_file_path = ''
+
+    def inspect_and_sort(self):
+        for dirpath, dirnames, filenames in os.walk(self.source_name):
+            for file in filenames:
+                self.full_file_path = os.path.join(dirpath, file)
+                secs = os.path.getmtime(self.full_file_path)
+                file_time = time.gmtime(secs)
+                self.dir_constructor(file,
+                                     file_time.tm_year,
+                                     file_time.tm_mon,
+                                     file_time.tm_mday)
+
+    def dir_constructor(self, file, year, month, day):
+        cpy_dir = f'{self.destination_name}/{str(year)}/{str(month)}/{str(day)}'
+        cpy_dir = os.path.normpath(cpy_dir)
+        if not os.path.exists(cpy_dir):
+            os.makedirs(cpy_dir)
+        result_file_path = f'{cpy_dir}/{file}'
+        self.move_file_to_dir(result_file_path)
+
+    def move_file_to_dir(self, result_file_path):
+        shutil.copy2(self.full_file_path, result_file_path)
+
+
+class ZipArrange(FileArrange):
+
+    def inspect_and_sort(self):
+        # для бробежки по архиву
+        # z_file = zipfile.ZipFile('icons.zip', 'r').infolist()
+        # for line in z_file:
+        #     # print(line.date_time)
+        pass
+
+    def move_file_to_dir(self, cpy_dir):
+        pass
+
+
+path = 'icons'
+
+file_arrange = FileArrange(path, 'icons_by_year')
+file_arrange.inspect_and_sort()
 
 # Усложненное задание (делать по желанию)
 # Нужно обрабатывать zip-файл, содержащий фотографии, без предварительного извлечения файлов в папку.
