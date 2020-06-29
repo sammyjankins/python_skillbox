66a7e65ec8430b27de7b4b23bec640168c3452f6
SammyJankins <neroragno@mail.ru>
2020-06-29 23:15:41 +0700

    Готов пятый элемент

--- lesson_010/01_fifth_element.py
+++ lesson_010/01_fifth_element.py
@@ -4,17 +4,24 @@
 
 BRUCE_WILLIS = 42
 
-input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
+input_data = None
+while True:
+    try:
+        input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+        leeloo = int(input_data[4])
+        result = BRUCE_WILLIS * leeloo
+    except ValueError:
+        print(f"- Вы люди такие странные! '{input_data[4]}' - ведь не число!")
+    except IndexError:
+        print(f"- Я была рождена, чтобы считать до 5, а не до {len(input_data)}...")
+    except Exception:
+        print(f"- БАДА БУМ!")
+    else:
+        print(f"- Leeloo Dallas! Multi-pass № {result}!")
+        break
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
 # - IndexError - выход за границы списка
 # - остальные исключения
 # для каждого типа исключений написать на консоль соотв. сообщение
-
-
-
-
