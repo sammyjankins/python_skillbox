a155bb4a63a913d7a40b2fe9f4855428e1f4b7e0
SammyJankins <neroragno@mail.ru>
2020-07-08 15:35:43 +0700

    11 модуль - 1 задание (готово)

--- lesson_011/01_shapes.py
+++ lesson_011/01_shapes.py
@@ -2,6 +2,7 @@
 
 import simple_draw as sd
 
+
 # На основе вашего кода из решения lesson_004/01_shapes.py сделать функцию-фабрику,
 # которая возвращает функции рисования треугольника, четырехугольника, пятиугольника и т.д.
 #
@@ -14,12 +15,26 @@ import simple_draw as sd
 
 
 def get_polygon(n):
-    pass
-    # TODO здесь ваш код
+    def draw_shapes(point, sides, angle_step, angle=0, length=200):
+        if sides is 1:
+            return point
+        v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+        v1.draw()
+        return draw_shapes(point=v1.end_point,
+                           sides=sides - 1,
+                           angle_step=angle_step,
+                           angle=angle + angle_step,
+                           length=length)
+
+    def shapes(point, angle=0, length=200):
+        angle_step = 360 / n
+        end_point = draw_shapes(point, n, angle_step, angle, length)
+        sd.line(point, end_point, width=3)
+
+    return shapes
 
 
-draw_triangle = get_polygon(n=3)
-draw_triangle(point=sd.get_point(200, 200), angle=13, length=100)
-
+for draw_shape in [get_polygon(n) for n in range(1, 8)]:
+    draw_shape(point=sd.random_point(), angle=13, length=100)
 
 sd.pause()
