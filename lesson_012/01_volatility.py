c8b3e994f822f6aadff391b60e29e26c17596a8f
SammyJankins <neroragno@mail.ru>
2020-07-17 20:31:48 +0700

    Модуль 12 - задание 1: на проверку перед вторым

--- lesson_012/01_volatility.py
+++ lesson_012/01_volatility.py
@@ -73,4 +73,72 @@
 #     def run(self):
 #         <обработка данных>
 
-# TODO написать код в однопоточном/однопроцессорном стиле
+from utils import time_track
+import os
+
+
+class VolCalc:
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.vol = 0
+        self.max_cost = 0
+        self.min_cost = 0
+        self.volatility = 0
+        self.ticker = ''
+
+    def half_sum(self):
+        return (self.max_cost + self.min_cost) / 2
+
+    def evaluate_vol(self):
+        return ((self.max_cost - self.min_cost) / self.half_sum()) * 100
+
+    def run(self):
+        self.ticker = self.file_name.split('_')[1][:-4]
+        with open(file=self.file_name, mode='r', encoding='utf8') as file:
+            transaction_prices = [float(line.split(',')[2]) for line in file if 'SECID' not in line]
+            transaction_prices.sort()
+            self.min_cost, self.max_cost = transaction_prices[0], transaction_prices[-1]
+            self.volatility = self.evaluate_vol()
+
+
+@time_track
+def main():
+    directory_to_scan = 'trades'
+    for dirpath, dirnames, filenames in os.walk(directory_to_scan):
+        vols = [VolCalc(os.path.join(directory_to_scan, file)) for file in filenames]
+        for vol in vols:
+            vol.run()
+
+        # формируем списки для принта:
+        vols.sort(key=lambda obj: obj.volatility)
+        zero_vols = []
+        while True:
+            if vols[0].volatility == 0:
+                zero_vols.append(vols.pop(0).ticker)
+            else:
+                break
+        max_vols = [vols.pop() for _ in range(3)]
+        min_vols = [vols.pop(0) for _ in range(3)]
+
+        # принтуем отформатированные списки:
+        print('Максимальная волатильность: ')
+        print('+----------+--------------+\n|  TICKER  |      VOL     |\n+----------+--------------+')
+
+        for value in max_vols:
+            print(f'|{value.ticker:^9} - {value.volatility:^10.2f} % |')
+        print('+----------+--------------+\n')
+
+        print('Минимальная волатильность: ')
+        print('+----------+--------------+\n|  TICKER  |      VOL     |\n+----------+--------------+')
+        for value in min_vols[::-1]:
+            print(f'|{value.ticker:^9} - {value.volatility:^10.2f} % |')
+        print('+----------+--------------+\n')
+
+        print('Нулевая волатильность: ')
+        print(', '.join([value for value in zero_vols]))
+        print()
+
+
+if __name__ == '__main__':
+    main()
