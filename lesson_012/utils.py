a6c668b2ef84dc6bef8aa1c9f9302335d668deb0
Максим <bi4ok23@gmail.com>
2020-07-19 22:01:58 +0300

    lesson 012: нужны правки в 03

--- lesson_012/utils.py
+++ lesson_012/utils.py
@@ -23,6 +23,7 @@ class VolDataProcessor:
         self.zero = []
         self.vols_container = vols_container
         self.process(max_tickers, min_tickers)
+        # TODO Основную логику программы стоит всё же запускать отдельно, не в Init-е
 
     # что-то мне подсказывает, что такое использование специального метода
     # не совсем корректно...если так, жду замечания)
@@ -43,6 +44,9 @@ class VolDataProcessor:
         self.max = [self.vols_container.pop() for _ in range(max_tickers)]
 
     def construct_min(self, min_tickers):
+        # TODO Удаление элемента из начала списка ведет к сдвигу всех элементов списка
+        # TODO т.е. вызывает количество операций, пропорциональное количеству элементов в списке
+        # TODO в целом эффективнее было бы использовать срезы из списков, например список[-3:]
         self.min = [self.vols_container.pop(0) for _ in range(min_tickers)]
 
     def process(self, max_tickers, min_tickers):
@@ -51,7 +55,9 @@ class VolDataProcessor:
         self.construct_max(max_tickers)
         self.construct_min(min_tickers)
 
-    def print_max(self):
+    def print_max(self):  # TODO Плюс на каждый метод сам по себе выделяется отдельная память
+        # TODO пусть это и не так затратно, как в ситуации со списком.
+        # TODO Но можно было бы объединить печать в один метод.
         print('Максимальная волатильность: ')
         print('+----------+--------------+\n|  TICKER  |      VOL     |\n+----------+--------------+')
 
@@ -75,6 +81,9 @@ class VolQueueProcessor(VolDataProcessor):
 
     def process(self, max_tickers, min_tickers):
         # это вообще законно?
+        # TODO В этом скорее нет необходимости, класс для тикера можно создать и вне функции/класса
+        # TODO правда не уверен, что это необходимо, т.к. по сути это лишние затраты памяти
+        # TODO когда для этой цели можно использовать тот же словарь или список
         class Vol:
 
             def __init__(self, ticker, volatility):
@@ -87,3 +96,4 @@ class VolQueueProcessor(VolDataProcessor):
             vols_list.append(vol)
         self.vols_container = vols_list
         super().process(max_tickers, min_tickers)
+# TODO И по итогу - чем больше кода вы сможете удалить, тем лучше он будет работать :)
